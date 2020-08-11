51aae4e241b5806209a781cac96a4af570e07905
Nameyusliadiyusof <yusliadiyusof@pipeline.com.my>
2020-08-11 17:21:12 +0800

    hdauhduahda

--- src/app/core/admin/kpi-module/kpi-module.component.ts
+++ src/app/core/admin/kpi-module/kpi-module.component.ts
@@ -242,62 +242,14 @@ export class KpiModuleComponent implements OnInit, OnDestroy {
     this.zone.runOutsideAngular(() => {
       this.getChart();
       this.getChart2();
+      this.getChart3();
+      this.getChart4();
     });
   }
 
   getChart() {
-    // let chart = am4core.create("chartScheduler", am4charts.XYChart);
-    let chart = am4core.create("chartScheduler1", am4charts.PieChart);
-
-    // Add data
-    chart.data = [
-      {
-        country: "bore pile",
-        litres: 501.9,
-      },
-      {
-        country: "Micro pile",
-        litres: 301.9,
-      },
-      {
-        country: "Crosshead",
-        litres: 201.1,
-      },
-      {
-        country: "Beam",
-        litres: 165.8,
-      },
-      {
-        country: "Parapet",
-        litres: 139.9,
-      },
-      {
-        country: "Caisson pile",
-        litres: 128.3,
-      },
-    ];
-
-    // Add and configure Series
-    let pieSeries = chart.series.push(new am4charts.PieSeries());
-    pieSeries.dataFields.value = "litres";
-    pieSeries.dataFields.category = "country";
-    pieSeries.slices.template.stroke = am4core.color("#fff");
-    pieSeries.slices.template.strokeOpacity = 1;
-
-    /// change
-    pieSeries.radius = 80;
-
-    // This creates initial animation
-    pieSeries.hiddenState.properties.opacity = 1;
-    pieSeries.hiddenState.properties.endAngle = -90;
-    pieSeries.hiddenState.properties.startAngle = -90;
-
-    chart.hiddenState.properties.radius = am4core.percent(0);
-  }
-
-  getChart2() {
     // Create chart instance
-    let chart = am4core.create("chartScheduler2", am4charts.XYChart);
+    let chart = am4core.create("chartKPI1", am4charts.XYChart);
     chart.scrollbarX = new am4core.Scrollbar();
 
     // Add data
@@ -334,37 +286,37 @@ export class KpiModuleComponent implements OnInit, OnDestroy {
         country: "Aug",
         visits: 711,
       },
-      {
-        country: "Sep",
-        visits: 665,
-      },
-      {
-        country: "Oct",
-        visits: 580,
-      },
-      {
-        country: "Nov",
-        visits: 443,
-      },
-      {
-        country: "Dec",
-        visits: 441,
-      },
     ];
+
+    prepareParetoData();
+
+    function prepareParetoData() {
+      let total = 0;
+
+      for (var i = 0; i < chart.data.length; i++) {
+        let value = chart.data[i].visits;
+        total += value;
+      }
+
+      let sum = 0;
+      for (var i = 0; i < chart.data.length; i++) {
+        let value = chart.data[i].visits;
+        sum += value;
+        chart.data[i].pareto = (sum / total) * 100;
+      }
+    }
 
     // Create axes
     let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
     categoryAxis.dataFields.category = "country";
     categoryAxis.renderer.grid.template.location = 0;
-    categoryAxis.renderer.minGridDistance = 30;
-    categoryAxis.renderer.labels.template.horizontalCenter = "right";
-    categoryAxis.renderer.labels.template.verticalCenter = "middle";
-    categoryAxis.renderer.labels.template.rotation = 270;
+    categoryAxis.renderer.minGridDistance = 60;
     categoryAxis.tooltip.disabled = true;
-    categoryAxis.renderer.minHeight = 110;
 
     let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
     valueAxis.renderer.minWidth = 50;
+    valueAxis.min = 0;
+    valueAxis.cursorTooltipEnabled = false;
 
     // Create series
     let series = chart.series.push(new am4charts.ColumnSeries());
@@ -390,7 +342,494 @@ export class KpiModuleComponent implements OnInit, OnDestroy {
       return chart.colors.getIndex(target.dataItem.index);
     });
 
+    let paretoValueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    paretoValueAxis.renderer.opposite = true;
+    paretoValueAxis.min = 0;
+    paretoValueAxis.max = 100;
+    paretoValueAxis.strictMinMax = true;
+    paretoValueAxis.renderer.grid.template.disabled = true;
+    paretoValueAxis.numberFormatter = new am4core.NumberFormatter();
+    paretoValueAxis.numberFormatter.numberFormat = "#'%'";
+    paretoValueAxis.cursorTooltipEnabled = false;
+
+    let paretoSeries = chart.series.push(new am4charts.LineSeries());
+    paretoSeries.dataFields.valueY = "pareto";
+    paretoSeries.dataFields.categoryX = "country";
+    paretoSeries.yAxis = paretoValueAxis;
+    paretoSeries.tooltipText = "pareto: {valueY.formatNumber('#.0')}%[/]";
+    paretoSeries.bullets.push(new am4charts.CircleBullet());
+    paretoSeries.strokeWidth = 2;
+    paretoSeries.stroke = new am4core.InterfaceColorSet().getFor(
+      "alternativeBackground"
+    );
+    paretoSeries.strokeOpacity = 0.5;
+
     // Cursor
+    chart.cursor = new am4charts.XYCursor();
+    chart.cursor.behavior = "panX";
+  }
+
+  getChart2() {
+    // Create chart instance
+    let chart = am4core.create("chartKPI2", am4charts.XYChart);
+
+    let data = [];
+
+    chart.data = [
+      {
+        year: "2014",
+        income: 23.5,
+        expenses: 21.1,
+        lineColor: chart.colors.next(),
+      },
+      {
+        year: "2015",
+        income: 26.2,
+        expenses: 30.5,
+      },
+      {
+        year: "2016",
+        income: 30.1,
+        expenses: 34.9,
+      },
+      {
+        year: "2017",
+        income: 20.5,
+        expenses: 23.1,
+      },
+      {
+        year: "2018",
+        income: 30.6,
+        expenses: 28.2,
+        lineColor: chart.colors.next(),
+      },
+      {
+        year: "2019",
+        income: 34.1,
+        expenses: 31.9,
+      },
+      {
+        year: "2020",
+        income: 34.1,
+        expenses: 31.9,
+      },
+      {
+        year: "2021",
+        income: 34.1,
+        expenses: 31.9,
+        lineColor: chart.colors.next(),
+      },
+      {
+        year: "2022",
+        income: 34.1,
+        expenses: 31.9,
+      },
+      {
+        year: "2023",
+        income: 34.1,
+        expenses: 31.9,
+      },
+      {
+        year: "2024",
+        income: 34.1,
+        expenses: 31.9,
+      },
+    ];
+
+    let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.ticks.template.disabled = true;
+    categoryAxis.renderer.line.opacity = 0;
+    categoryAxis.renderer.grid.template.disabled = true;
+    categoryAxis.renderer.minGridDistance = 40;
+    categoryAxis.dataFields.category = "year";
+    categoryAxis.startLocation = 0.4;
+    categoryAxis.endLocation = 0.6;
+
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.tooltip.disabled = true;
+    valueAxis.renderer.line.opacity = 0;
+    valueAxis.renderer.ticks.template.disabled = true;
+    valueAxis.min = 0;
+
+    let lineSeries = chart.series.push(new am4charts.LineSeries());
+    lineSeries.dataFields.categoryX = "year";
+    lineSeries.dataFields.valueY = "income";
+    lineSeries.tooltipText = "revenue: {valueY.value}";
+    lineSeries.fillOpacity = 0.5;
+    lineSeries.strokeWidth = 3;
+    lineSeries.propertyFields.stroke = "lineColor";
+    lineSeries.propertyFields.fill = "lineColor";
+
+    let bullet = lineSeries.bullets.push(new am4charts.CircleBullet());
+    bullet.circle.radius = 6;
+    bullet.circle.fill = am4core.color("#fff");
+    bullet.circle.strokeWidth = 3;
+
+    chart.cursor = new am4charts.XYCursor();
+    chart.cursor.behavior = "panX";
+    chart.cursor.lineX.opacity = 0;
+    chart.cursor.lineY.opacity = 0;
+
+    chart.scrollbarX = new am4core.Scrollbar();
+    chart.scrollbarX.parent = chart.bottomAxesContainer;
+  }
+
+  getChart3() {
+    // Create chart instance
+    let chart = am4core.create("chartKPI3", am4charts.XYChart);
+
+    // Add data
+    chart.data = [
+      {
+        year: "Jan",
+        italy: 10000,
+        germany: 50000,
+        uk: 30000,
+      },
+      {
+        year: "Feb",
+        italy: 10000,
+        germany: 20000,
+        uk: 60000,
+      },
+      {
+        year: "Mar",
+        italy: 20000,
+        germany: 30000,
+        uk: 10000,
+      },
+      {
+        year: "Apr",
+        italy: 30000,
+        germany: 40000,
+        uk: 10000,
+      },
+      {
+        year: "May",
+        italy: 50000,
+        germany: 10000,
+        uk: 20000,
+      },
+      {
+        year: "Jun",
+        italy: 30000,
+        germany: 20000,
+        uk: 10000,
+      },
+      {
+        year: "Jul",
+        italy: 10000,
+        germany: 20000,
+        uk: 30000,
+      },
+      {
+        year: "Aug",
+        italy: 20000,
+        germany: 10000,
+        uk: 50000,
+      },
+    ];
+
+    // Create category axis
+    let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "year";
+    categoryAxis.renderer.opposite = true;
+
+    // Create value axis
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.renderer.inversed = true;
+    // valueAxis.title.text = "Place taken";
+    valueAxis.renderer.minLabelPosition = 0.01;
+
+    // Create series
+    let series1 = chart.series.push(new am4charts.LineSeries());
+    series1.dataFields.valueY = "italy";
+    series1.dataFields.categoryX = "year";
+    series1.name = "Cost";
+    series1.bullets.push(new am4charts.CircleBullet());
+    series1.tooltipText = "{name} in {categoryX}: {valueY}";
+    series1.legendSettings.valueText = "{valueY}";
+    series1.visible = false;
+
+    // let series2 = chart.series.push(new am4charts.LineSeries());
+    // series2.dataFields.valueY = "germany";
+    // series2.dataFields.categoryX = "year";
+    // series2.name = "Utilization";
+    // series2.bullets.push(new am4charts.CircleBullet());
+    // series2.tooltipText = "{name} in {categoryX}: {valueY}";
+    // series2.legendSettings.valueText = "{valueY}";
+
+    // Add chart cursor
+    chart.cursor = new am4charts.XYCursor();
+    chart.cursor.behavior = "zoomY";
+
+    let hs1 = series1.segments.template.states.create("hover");
+    hs1.properties.strokeWidth = 5;
+    series1.segments.template.strokeWidth = 1;
+
+    // let hs2 = series2.segments.template.states.create("hover");
+    // hs2.properties.strokeWidth = 5;
+    // series2.segments.template.strokeWidth = 1;
+
+    // Add legend
+    chart.legend = new am4charts.Legend();
+    // chart.legend.itemContainers.template.events.on("over", function (event) {
+    //   let segments = event.target.dataItem.dataContext.segments;
+    //   segments.each(function (segment) {
+    //     segment.isHover = true;
+    //   });
+    // });
+
+    // chart.legend.itemContainers.template.events.on("out", function (event) {
+    //   let segments = event.target.dataItem.dataContext.segments;
+    //   segments.each(function (segment) {
+    //     segment.isHover = false;
+    //   });
+    // });
+  }
+
+  getChart4() {
+    let chart = am4core.create("chartKPI4", am4charts.XYChart);
+
+    // Add data
+    chart.data = [
+      {
+        year: "1950",
+        value: -0.307,
+      },
+      {
+        year: "1951",
+        value: -0.168,
+      },
+      {
+        year: "1952",
+        value: -0.073,
+      },
+      {
+        year: "1953",
+        value: -0.027,
+      },
+      {
+        year: "1954",
+        value: -0.251,
+      },
+      {
+        year: "1955",
+        value: -0.281,
+      },
+      {
+        year: "1956",
+        value: -0.348,
+      },
+      {
+        year: "1957",
+        value: -0.074,
+      },
+      {
+        year: "1958",
+        value: -0.011,
+      },
+      {
+        year: "1959",
+        value: -0.074,
+      },
+      {
+        year: "1960",
+        value: -0.124,
+      },
+      {
+        year: "1961",
+        value: -0.024,
+      },
+      {
+        year: "1962",
+        value: -0.022,
+      },
+      {
+        year: "1963",
+        value: 0,
+      },
+      {
+        year: "1964",
+        value: -0.296,
+      },
+      {
+        year: "1965",
+        value: -0.217,
+      },
+      {
+        year: "1966",
+        value: -0.147,
+      },
+      {
+        year: "1967",
+      },
+      {
+        year: "1971",
+        value: -0.19,
+      },
+      {
+        year: "1972",
+        value: -0.056,
+      },
+      {
+        year: "1973",
+        value: 0.077,
+      },
+      {
+        year: "1974",
+        value: -0.213,
+      },
+      {
+        year: "1975",
+        value: -0.17,
+      },
+      {
+        year: "1976",
+        value: -0.254,
+      },
+      {
+        year: "1977",
+        value: 0.019,
+      },
+      {
+        year: "1978",
+        value: -0.063,
+      },
+      {
+        year: "1979",
+        value: 0.05,
+      },
+      {
+        year: "1980",
+        value: 0.077,
+      },
+      {
+        year: "1981",
+        value: 0.12,
+      },
+      {
+        year: "1982",
+        value: 0.011,
+      },
+      {
+        year: "1983",
+        value: 0.177,
+      },
+      {
+        year: "1984",
+      },
+      {
+        year: "1989",
+        value: 0.104,
+      },
+      {
+        year: "1990",
+        value: 0.255,
+      },
+      {
+        year: "1991",
+        value: 0.21,
+      },
+      {
+        year: "1992",
+        value: 0.065,
+      },
+      {
+        year: "1993",
+        value: 0.11,
+      },
+      {
+        year: "1994",
+        value: 0.172,
+      },
+      {
+        year: "1995",
+        value: 0.269,
+      },
+      {
+        year: "1996",
+        value: 0.141,
+      },
+      {
+        year: "1997",
+        value: 0.353,
+      },
+      {
+        year: "1998",
+        value: 0.548,
+      },
+      {
+        year: "1999",
+        value: 0.298,
+      },
+      {
+        year: "2000",
+        value: 0.267,
+      },
+      {
+        year: "2001",
+        value: 0.411,
+      },
+      {
+        year: "2002",
+        value: 0.462,
+      },
+      {
+        year: "2003",
+        value: 0.47,
+      },
+      {
+        year: "2004",
+        value: 0.445,
+      },
+      {
+        year: "2005",
+        value: 0.47,
+      },
+    ];
+
+    // Create axes
+    let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.minGridDistance = 50;
+    dateAxis.renderer.grid.template.location = 0.5;
+    dateAxis.baseInterval = {
+      count: 1,
+      timeUnit: "year",
+    };
+
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+    // Create series
+    let series = chart.series.push(new am4charts.LineSeries());
+    series.dataFields.valueY = "value";
+    series.dataFields.dateX = "year";
+    series.strokeWidth = 3;
+    series.connect = false;
+    series.tensionX = 0.8;
+    series.fillOpacity = 0.2;
+    let bullet = series.bullets.push(new am4charts.CircleBullet());
+    bullet.stroke = new am4core.InterfaceColorSet().getFor("background");
+    bullet.strokeWidth = 2;
+    bullet.tooltipText = "{valueY}";
+    bullet.circle.radius = 4;
+
+    bullet.adapter.add("fill", function (fill, target) {
+      // if (target.dataItem.valueY > 0) {
+      //   return chart.colors.getIndex(2);
+      // }
+      return fill;
+    });
+
+    let range = valueAxis.createSeriesRange(series);
+    range.value = 0;
+    range.endValue = 100;
+    range.contents.stroke = chart.colors.getIndex(2);
+    range.contents.fill = range.contents.stroke;
+    range.contents.fillOpacity = 0.2;
+
+    chart.scrollbarX = new am4core.Scrollbar();
     chart.cursor = new am4charts.XYCursor();
   }
 }
